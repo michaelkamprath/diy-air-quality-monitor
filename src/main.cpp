c08db5e8599304663f814fd5f198ffcc24d01462
Michael Kamprath <michael@kamprath.net>
2020-09-30 22:05:26 -0700

    added basic web servr to view AQI

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,18 @@
 #include <Arduino.h>
 #include "Application.h"
 
-Application* app = nullptr;
-
 void setup()
 {
   // Used for debug output only
   Serial.begin(9600);
-  app = new Application();
-
+  
+  Application::getInstance()->setup();
 }
 
 
 uint32_t loopCounter = 0;
 void loop()
 {
-  loopCounter++;
-
-  // slow down the call to the app loop some. 
-  if (loopCounter%1000 == 0) {
-    app->loop();
-  }
+  Application::getInstance()->loop();
 }
 
